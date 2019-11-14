f37c76a9d510e1b4dc06e32f8d72b23aa1012a28
Trevor Martin <astrabrauer@Trevors-MacBook-Pro.local>
2019-11-14 14:00:51 -0500

    edited TP_LSTM.py

--- models/TP_LSTM.py
+++ models/TP_LSTM.py
@@ -32,6 +32,7 @@ n_classes = 10
 # n_input by n_steps pixels, n_classes = num types of signatures (num people)
 
 
+# ADD COMMENTS EXPLAINING WHAT IS HAPPENING
 x = tf.placeholder("float", [None, n_steps, n_input])
 y = tf.placeholder("float", [None, n_classes]
 weights = {
@@ -41,11 +42,13 @@ biases = {
    'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
+
 def RNN(x, weights, biases):
    x = tf.unstack(x, n_steps, 1)
    lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
    outputs, states = rnn.static_rnn(lstm_cell, x, dtype = tf.float32)
    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
 
 pred = RNN(x, weights, biases)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = pred, labels = y))
@@ -53,6 +56,8 @@ optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
 correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 init = tf.global_variables_initializer()
+
+
 with tf.Session() as sess:
    sess.run(init)
    step = 1
@@ -73,19 +78,3 @@ with tf.Session() as sess:
    test_label = mnist.test.labels[:test_len]
    print("Testing Accuracy:", \
       sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
